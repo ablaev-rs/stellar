44e5b2e1cbdc96f0e34b1c15c73a2a0a05f836c2
Rustem Ablaev <ablaev.r.s@gmail.com>
2020-06-01 16:19:39 +0300

    refactor: add header

--- components/Header/index.js
+++ components/Header/index.js
@@ -1,15 +1,23 @@
 import React from 'react'
 import { observer } from 'startupjs'
 import './index.styl'
-import { Text } from 'react-native'
-import { Div, Span, Button, Br, Row, Card } from '@startupjs/ui'
-import { faPlus, faMinus } from '@fortawesome/free-solid-svg-icons'
+import { Image } from 'react-native'
+import { Div, H1, Span } from '@startupjs/ui'
+import { BASE_URL } from '@env'
 
 export default observer(function Header () {
+  const logoUrl = '/img/logo.png'
+  const base = BASE_URL
+
   return pug`
     Div.root
-      Div.logo
-      Div.name
-      Div.slogan
+      Div.container
+        Image.image(
+          source={uri: base + logoUrl}
+        )
+        H1.header Stellar
+        Span.slogan Just another free, fully responsive site template
+        Span.slogan built by @ajlkn for HTML5 UP.
+
   `
 })
