7541bdb0ae7ef97db0823f2dce5907cc5c30c21c
Rustem Ablaev <ablaev.r.s@gmail.com>
2020-06-01 18:27:41 +0300

    refactor: add navigation

--- components/Content/Navigation/index.js
+++ components/Content/Navigation/index.js
@@ -2,12 +2,16 @@ import React from 'react'
 import { observer } from 'startupjs'
 import './index.styl'
 import { Text } from 'react-native'
-import { Div, Span, Button, Br, Row, Card } from '@startupjs/ui'
+import { Div, Menu } from '@startupjs/ui'
 import { faPlus, faMinus } from '@fortawesome/free-solid-svg-icons'
 
 export default observer(function Header () {
+  const navigation = ['Introduction', 'First Section', 'Second Section', 'Get Started']
   return pug`
     Div.root
-      Text i am here
+      Menu.nav(variant='horizontal')
+        each val in navigation
+          Menu.item= val
+      
   `
 })
--- components/Content/index.js
+++ components/Content/index.js
@@ -1,16 +1,16 @@
 import React from 'react'
 import { observer } from 'startupjs'
 import './index.styl'
+import { Text } from 'react-native'
 import { Div } from '@startupjs/ui'
 import { faPlus, faMinus } from '@fortawesome/free-solid-svg-icons'
-import Header from '../Header'
-import Content from '../Content'
+import Navigation from './Navigation'
 
-export default observer(function Main ({ style }) {
+export default observer(function Header () {
   return pug`
-    Div.root(style=style)
-      Div.templateParams
-        Header
-        Content
+    Div.root
+      Div.container
+        Navigation
+
   `
 })
