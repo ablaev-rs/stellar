b2195153664706ee215256a564441001979930e0
Rustem Ablaev <ablaev.r.s@gmail.com>
2020-06-04 13:20:55 +0300

    fix: buttons

--- components/Content/Second/index.js
+++ components/Content/Second/index.js
@@ -38,27 +38,25 @@ export default observer(function Second () {
 
   return pug`
     View.root
-      View.container
+      View.infoBox
+        Text.h2 Ipsum consequat
+        Hr.sep
+        Text.text Donec imperdiet consequat consequat. Suspendisse feugiat congue posuere. Nulla massa urna, fermentum eget quam aliquet.
 
-        View.infoBox
-          Text.h2 Ipsum consequat
-          Hr.sep
-          Text.text Donec imperdiet consequat consequat. Suspendisse feugiat congue posuere. Nulla massa urna, fermentum eget quam aliquet.
+      View.items
+        each value in data
+          View.item(styleName = value.explanation.toLowerCase())
+            View.imageBox
+              Image.image(
+                source={uri: base + value.image}
+              )
+            View.count #{value.count}
+            View.explanation #{value.explanation}
 
-        View.items
-          each value in data
-            View.item(styleName = value.explanation.toLowerCase())
-              View.imageBox
-                Image.image(
-                  source={uri: base + value.image}
-                )
-              View.count #{value.count}
-              View.explanation #{value.explanation}
-
-        Text.text.justify Nam elementum nisl et mi a commodo porttitor. Morbi sit amet nisl eu arcu faucibus hendrerit vel a risus. Nam a orci mi, elementum ac arcu sit amet, fermentum pellentesque et purus. Integer maximus varius lorem, sed convallis diam accumsan sed. Etiam porttitor placerat sapien, sed eleifend a enim pulvinar faucibus semper quis ut arcu. Ut non nisl a mollis est efficitur vestibulum. Integer eget purus nec nulla mattis et accumsan ut magna libero. Morbi auctor iaculis porttitor. Sed ut magna ac risus et hendrerit scelerisque. Praesent eleifend lacus in lectus aliquam porta. Cras eu ornare dui curabitur lacinia.
-        
-        View.btnWrapper
-          Button(action = 'Learn More')
+      Text.text.justify Nam elementum nisl et mi a commodo porttitor. Morbi sit amet nisl eu arcu faucibus hendrerit vel a risus. Nam a orci mi, elementum ac arcu sit amet, fermentum pellentesque et purus. Integer maximus varius lorem, sed convallis diam accumsan sed. Etiam porttitor placerat sapien, sed eleifend a enim pulvinar faucibus semper quis ut arcu. Ut non nisl a mollis est efficitur vestibulum. Integer eget purus nec nulla mattis et accumsan ut magna libero. Morbi auctor iaculis porttitor. Sed ut magna ac risus et hendrerit scelerisque. Praesent eleifend lacus in lectus aliquam porta. Cras eu ornare dui curabitur lacinia.
+      
+      View.btnWrapper
+        Button(action = 'Learn More')
 
   `
 })
