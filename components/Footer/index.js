3676bead7c499d5566bacb1596b8f6ec4f9ebd55
Rustem Ablaev <ablaev.r.s@gmail.com>
2020-06-04 13:23:45 +0300

    fix: wrapper footer

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -17,40 +17,38 @@ export default observer(function Header () {
 
   return pug`
     View.root
-      View.container
+      View.footerBox
+        View.infoBox
+          Text.h2 Aliquam sed mauris
+          Text.text Sed lorem ipsum dolor sit amet et nullam consequat feugiat consequat magna adipiscing tempus etiam dolore veroeros. eget dapibus mauris. Cras aliquet, nisl ut viverra sollicitudin, ligula erat egestas velit, vitae tincidunt odio.
+          View.btnWrapper
+            Button(action = 'Learn More' addStyle = 'btnPink')
 
-        View.footerBox
-          View.infoBox
-            Text.h2 Aliquam sed mauris
-            Text.text Sed lorem ipsum dolor sit amet et nullam consequat feugiat consequat magna adipiscing tempus etiam dolore veroeros. eget dapibus mauris. Cras aliquet, nisl ut viverra sollicitudin, ligula erat egestas velit, vitae tincidunt odio.
-            View.btnWrapper
-              Button(action = 'Learn More' addStyle = 'btnPink')
+        View.contact
+          Text.h2 Etiam feugiat
+          View.contactBox
+            View.contactData 
+              Text.title Address
+              Text.value 1234 Somewhere Road • Nashville, TN 00000 • USA
+            View.contactData 
+              Text.title Phone
+              Text.value (000) 000-0000 x 0000
+            View.contactData 
+              Text.title Email
+              Text(onPress=() => Linking.openURL('#')).value.link information@untitled.tld
 
-          View.contact
-            Text.h2 Etiam feugiat
-            View.contactBox
-              View.contactData 
-                Text.title Address
-                Text.value 1234 Somewhere Road • Nashville, TN 00000 • USA
-              View.contactData 
-                Text.title Phone
-                Text.value (000) 000-0000 x 0000
-              View.contactData 
-                Text.title Email
-                Text(onPress=() => Linking.openURL('#')).value.link information@untitled.tld
+          View.social
+            each url, index in icons
+              View.imageBorder(styleName=index === 0 ? 'first' : '')
+                Image.image(
+                  source={uri: base + url}
+                )
 
-            View.social
-              each url, index in icons
-                View.imageBorder(styleName=index === 0 ? 'first' : '')
-                  Image.image(
-                    source={uri: base + url}
-                  )
-
-        View.copyright
-          Text.text © Untitled. Design: 
-          Text(onPress=() => Linking.openURL('#')).text.link HTML5 UP 
-          Text.text Demo Images: 
-          Text(onPress=() => Linking.openURL('#')).text.link Unsplash.
+      View.copyright
+        Text.text © Untitled. Design: 
+        Text(onPress=() => Linking.openURL('#')).text.link HTML5 UP 
+        Text.text Demo Images: 
+        Text(onPress=() => Linking.openURL('#')).text.link Unsplash.
 
   `
 })
