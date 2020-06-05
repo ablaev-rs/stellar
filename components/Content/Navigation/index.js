b4e2964ef1d9cda3faec59c18e58989172c37138
Rustem Ablaev <ablaev.r.s@gmail.com>
2020-06-05 16:27:42 +0300

    fix: reviews

--- components/Content/Navigation/index.js
+++ components/Content/Navigation/index.js
@@ -1,16 +1,15 @@
 import React from 'react'
 import { observer } from 'startupjs'
 import './index.styl'
-import { View } from 'react-native'
-import { Menu } from '@startupjs/ui'
+import { View, Text } from 'react-native'
 
 export default observer(function Header () {
   const navigation = ['Introduction', 'First Section', 'Second Section', 'Get Started']
   return pug`
     View.root
-      Menu.nav(variant='horizontal')
-        each val, index in navigation
-          Menu.item(key = index)= val
+      each val, index in navigation
+        View.item(key = index)
+          Text.text #{val}
       
   `
 })
