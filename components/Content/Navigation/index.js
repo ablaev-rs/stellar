e34404a0b19a38269c5bfa95a49d5fb31947e2d5
Rustem Ablaev <ablaev.r.s@gmail.com>
2020-06-10 11:31:45 +0300

    fix: introduction

--- components/Content/Navigation/index.js
+++ components/Content/Navigation/index.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import { observer } from 'startupjs'
 import './index.styl'
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity } from 'react-native'
 
 export default observer(function Header () {
   const navigation = ['Introduction', 'First Section', 'Second Section', 'Get Started']
   return pug`
     View.root
       each val, index in navigation
-        View.item(key = index styleName=index === 0 ? 'first' : '')
+        TouchableOpacity.item(key = index styleName=index === 0 ? 'first' : '')
           Text.text #{val}
       
   `
